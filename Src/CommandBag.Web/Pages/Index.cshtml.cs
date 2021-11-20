ffc28c7f53c23154cd9c2433b2762e99b4a619be
IMujagic <ismar.mujagic@gmail.com>
2021-11-20 16:22:44 +0100

    add web presentation layer and list available commands on index page

---
+++ Src/CommandBag.Web/Pages/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using CommandBag.Core;
+using CommandBag.DIConfig;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommandBag.Web.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger, ICommandRunner runner)
+        {
+            _logger = logger;
+        }
+
+        public List<CommandMetadata> Commands { get; private set; }
+
+        public void OnGet()
+        {
+            this.Commands = MainModule.CommandMetadataList;
+        }
+    }
+}

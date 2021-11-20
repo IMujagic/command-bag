11bd99069ffedea16275e458aed9d371a156e990
IMujagic <ismar.mujagic@gmail.com>
2021-11-20 16:34:02 +0100

    add CopyFile example command

---
+++ Src/CommandBag.Commands/File/CopyFileCommand.cs
@@ -0,0 +1,19 @@
+﻿using CommandBag.Common;
+using CommandBag.Core;
+using CommandBag.Core.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommandBag.Commands.File
+{
+    [CommandGroup("File")]
+    [CommandDescription("Copy files from source to target location")]
+    public class CopyFileCommand : IDomainCommand<CopyFilePayload>
+    {
+        public Result Execute(CopyFilePayload payload)
+        {
+            return Result.Ok();
+        }
+    }
+}
---
+++ Src/CommandBag.Commands/File/CopyFilePayload.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommandBag.Commands.File
+{
+    public class CopyFilePayload
+    {
+        public string SourcePath { get; set; }
+        public string TargetPath { get; set; }
+    }
+}
